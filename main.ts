66a824d511ac7cfbcb68958abb70648fa3308035
tzwrtzis <tzwrtzis@gmail.com>
2024-12-07 15:23:56 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,1 @@
-basic.showLeds(`
-    . # . . .
-    . # . . .
-    . # . . .
-    . # . . .
-    . # # # .
-    `)
-music.play(music.stringPlayable("C5 B A G F G A B ", 90), music.PlaybackMode.UntilDone)
-music.play(music.stringPlayable("E D E D E D E D ", 90), music.PlaybackMode.UntilDone)
-basic.showLeds(`
-    # # # . .
-    # . . . .
-    # # . . .
-    # . . . .
-    # . . . .
-    `)
-music.play(music.stringPlayable("C5 B A G F G A B ", 90), music.PlaybackMode.UntilDone)
-music.play(music.stringPlayable("G B A G C5 B A B ", 90), music.PlaybackMode.UntilDone)
+
