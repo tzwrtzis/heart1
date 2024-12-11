f3ec372764afe0d1a73d9fdd5c19a73b92960165
tzwrtzis <tzwrtzis@gmail.com>
2024-12-11 20:16:32 +0100

    melody and icon

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showLeds(`
+    # . # . #
+    . # . # .
+    # # # # #
+    . # . # .
+    . # . # .
+    `)
+music.play(music.stringPlayable("G E B G B D G C ", 120), music.PlaybackMode.UntilDone)
